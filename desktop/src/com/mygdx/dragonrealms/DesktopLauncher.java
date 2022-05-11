e0ae75e429563a9797fac54b2b6528456aa0773c
Dominik Misztal <dominikmisztal@onet.eu>
2022-05-11 17:10:07 +0200

    test droplet based game commit, fixed names

--- desktop/src/com/mygdx/dragonrealms/DesktopLauncher.java
+++ desktop/src/com/mygdx/dragonrealms/DesktopLauncher.java
@@ -9,7 +9,9 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
+		config.setTitle("WaterDroplets");
+		config.setWindowedMode(800,480);
+		config.useVsync(true);
 		new Lwjgl3Application(new DragonRealms(), config);
 	}
 }
