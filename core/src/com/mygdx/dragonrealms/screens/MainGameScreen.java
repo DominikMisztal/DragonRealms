b65ae2f308acdab4ff08862d3fc824b6ae57314d
Dominik Misztal <dominikmisztal@onet.eu>
2022-05-28 20:15:59 +0200

    code cleanup

--- core/src/com/mygdx/dragonrealms/screens/MainGameScreen.java
+++ core/src/com/mygdx/dragonrealms/screens/MainGameScreen.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
-// import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
