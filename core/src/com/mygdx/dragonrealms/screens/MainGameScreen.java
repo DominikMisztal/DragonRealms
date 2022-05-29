b9c1051fe967f310292e2a7614f7d09bd2227cf2
Dominik Misztal <dominikmisztal@onet.eu>
2022-05-29 18:45:39 +0200

    Adjusted zoom values

--- core/src/com/mygdx/dragonrealms/screens/MainGameScreen.java
+++ core/src/com/mygdx/dragonrealms/screens/MainGameScreen.java
@@ -435,8 +435,8 @@ public class MainGameScreen extends ApplicationAdapter implements InputProcessor
     @Override
     public boolean scrolled(float amountX, float amountY) {
         camera.zoom += amountY/50;
-        if(camera.zoom > 1.1375){
-            camera.zoom = 1.1375f;
+        if(camera.zoom > 1.412){
+            camera.zoom = 1.412f;
         }
         if(camera.zoom < 0.6){
             camera.zoom = 0.6f;
